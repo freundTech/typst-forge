0c7314f8480f6211b1bd06d442e0dee26a6a5f20
Adrian Freund <adrian@freund.io>
2024-10-11 22:30:02 +0200

    fixup! Test packages in seperate ci job

--- matryoshka/build.sh
+++
@@ -1,5 +0,0 @@
-cargo build --locked --release --target wasm32-unknown-unknown
-
-mkdir -p "$PREFIX/share/typst/packages/typst-forge/${{ name }}/${{ version }}"
-cp typst.toml README.md lib.typ target/wasm32-unknown-unknown/release/matryoshka.wasm
-    "$PREFIX/share/typst/packages/typst-forge/${{ name }}/${{ version }}"
